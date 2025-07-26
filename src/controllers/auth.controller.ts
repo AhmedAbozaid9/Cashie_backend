a919579e10b2ed6e69c5a88be335b3c0b67bcbcc
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-26 16:42:27 +0300

    adds prisma

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,10 +2,11 @@ import { Request, Response } from "express";
 
 export const login = (req: Request, res: Response) => {
   const { email, password } = req.body;
-  
+  console.log(`Email: ${email}, Password: ${password}`);
   return res.status(200).json({ message: "Login successful" });
 };
 
 export const register = (req: Request, res: Response) => {
+  console.log(req.body);
   return res.status(201).json({ message: "Registration successful" });
 };
