abff100acc08bbd1418c61041bc98f2a003325aa
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-26 11:53:31 +0300

    validates login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate";
+import { loginSchema } from "../validation/LoginSchema";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", validate(loginSchema), login);
 router.post("/register", register);
 
 export default router;
