a20b973c422a1c8b2828023d3913ecc221b8172f
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-26 11:56:41 +0300

    adds register schema

---
+++ src/validation/RegisterSchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const loginSchema = z
+  .object({
+    name: z.string("Name is required").min(1, "Name is required"),
+    email: z
+      .email("Please enter a valid email address")
+      .min(1, "Email is required"),
+    password: z
+      .string("Password is required")
+      .min(8, "Password must be at least 8 characters long"),
+    passwordConfirmation: z
+      .string("Password confirmation is required")
+      .min(8, "Password confirmation must be at least 8 characters long"),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  });
