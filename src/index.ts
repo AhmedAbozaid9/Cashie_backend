2384a15a08f652ce6c809d4a92bbf3ec7b967806
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-29 22:15:20 +0300

    updates server to listen on all interfaces and parses port as integer; adds GitToolBox project settings

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT || "8080")
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
