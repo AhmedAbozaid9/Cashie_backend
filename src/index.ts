4d58f782e058ac140f6018adbadfd67d67d77a3e
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-29 22:08:11 +0300

    changes default server port to 8080

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
