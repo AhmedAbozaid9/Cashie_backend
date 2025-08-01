a3e4dfb6cb4bc755f7e33a4a92c4515f6985d5f6
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-01 11:11:00 +0300

    changes the routing to start with /api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 const app = express();
-const PORT = parseInt(process.env.PORT || "8080")
+const PORT = parseInt(process.env.PORT || "8080");
 
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-}).on("error", (err) => {
+app
+  .listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
     console.error("Error starting server:", err);
-})
+  });
