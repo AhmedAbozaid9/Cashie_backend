2be30fe05b4006ae8c0c4d08b7151bd4d6a78149
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-09 14:19:51 +0300

    fixes cors errors

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ const PORT = parseInt(process.env.PORT || "8080");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/accounts", auth, accountRoutes);
 
