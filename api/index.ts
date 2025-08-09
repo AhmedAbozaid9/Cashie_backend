ada8d2d2a2b5a20762a7e244656d63a9158f2dd1
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-09 16:49:32 +0300

    fixes deployment

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,10 @@ let app: any;
 try {
   // When built on Vercel (vercel-build runs `tsc`), use compiled output
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  app = require("../dist/api/index.js").default;
+  app = require("../dist/index.js").default;
 } catch {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  app = require("../src/api/index").default;
+  app = require("../src/index").default;
 }
 
 export default app as any;
